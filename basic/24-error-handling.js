50b577068ad5a179f2f9b066a6e38a254efa3231
unknown <dradefensasiciliana@gmail.com>
2026-03-07 20:24:58 -0600

    Manejo de errores

---
+++ basic/24-error-handling.js
@@ -0,0 +1,45 @@
+//Excepcion
+// Produce una excepcion
+let myObject 
+
+//Captura de errores
+//try...catch
+try {
+    console.log(myObject.name)
+}catch (error) {
+    console.log('Se ha producido un error')
+}
+
+// Captura de errores
+try {
+    console.log(myObject.name)
+}catch (error) {
+    console.log('Se ha producido un error')
+    console.log(error.message)
+}
+
+// Finally
+try {
+    console.log(myObject.name)
+}catch (error) {
+    console.log('Se ha producido un error')
+    console.log(error.message)
+}finally {
+    console.log('Esto se ejecuta siempre')
+}
+
+// Lanzamiento de errores
+// throw
+function sum(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Los argumentos deben ser números')
+    }
+    return a + b
+}
+
+sum(2, '3') // Esto lanzará un error
+
+//throw new Error('Esto es un error personalizado')
+
+
+
