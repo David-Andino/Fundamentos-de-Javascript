07a04f292e77d5aad0e68fbf40af17050046cd40
unknown <dradefensasiciliana@gmail.com>
2026-03-14 20:57:56 -0600

    Arrow Functions (Funciones Avanzadas)

--- Intermediate/00-advance-functions.js
+++ Intermediate/00-advance-functions.js
@@ -19,3 +19,23 @@ processGreating(greet, "Ricardo");
 
 const greet2 = returnGreeting("Andino");
 greet2("Andino");
+
+// Arrow Functions Avanzadas
+// Return implícito
+ 
+const multiply = (a, b) =>  a * b;
+console.log(multiply(5, 3));
+
+// - This Lexico
+const handler = {
+    name: "David",
+    greeting: function() {
+        console.log(`Hola ${this.name}`)
+    },
+    arrowGreeting: () => {
+        console.log(`Hola ${this.name}`)
+    }
+}
+
+handler.greeting();
+handles.arrowGreeting(); // undefined, porque el this en arrow functions no se refiere al objeto handler, sino al contexto global.
