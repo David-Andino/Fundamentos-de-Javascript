542e6b17f5547c027184ba00357589c8660df611
unknown <dradefensasiciliana@gmail.com>
2026-03-23 22:56:48 -0600

    Ejercicios 2

--- Intermediate/01-advanced-functions-exercise.js
+++ Intermediate/01-advanced-functions-exercise.js
@@ -5,3 +5,16 @@ function createGreeter(greeting) {
     }}
 const greetHello = createGreeter("Hola");
 greetHello("David");
+
+// 2. Implementa una funcion currificada que multiplique 3 numeros
+function multiply(a) {
+    return function(b) {
+        return function(c) {
+            return a * b * c;
+        }
+    }
+}
+const multiplyBy2 = multiply(2);
+const multiplyBy2And3 = multiplyBy2(3);
+const result = multiplyBy2And3(4);
+console.log(result); // Output: 24
